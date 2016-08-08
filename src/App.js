cd192b449e0e06e29b38aa7fdf47519ef4777162
Nicholas Palenchar <nick@Nicks-pro.local>
2016-08-08 15:13:08 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="splash-header App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h1>discover your true alliance</h1>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
