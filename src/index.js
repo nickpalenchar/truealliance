7821cea2c71d33a75b69b32a279f638ea30a6427
Nicholas Palenchar <nick@Nicks-pro.local>
2016-08-08 16:54:17 -0400

    setting basic login flow

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/index.css';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import Register from './smartComponents/pages/register/Register';
+import Home from './smartComponents/Home';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Register} />
+      <Route path="step" component={Home}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
