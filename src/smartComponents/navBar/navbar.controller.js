913140892921f3c6d57a7cc94c5507258db9cd74
Nicholas Palenchar <nick@Nicks-pro.local>
2016-10-09 21:19:28 -0400

    commit

--- src/smartComponents/navBar/navbar.controller.js
+++ src/smartComponents/navBar/navbar.controller.js
@@ -1,8 +1,25 @@
+import $ from 'jquery';
+import env from '../../env';
+const BACKEND_URL = env.BACKEND_URL;
+
+import { getMe } from '../../helpers/getMe';
 
 export function clearAllData() {
   if(confirm("Are you sure?")) {
-    window.localStorage.clear();
-    window.sessionStorage.clear();
-    window.location.href = "/";
+    debugger;
+    var me = getMe();
+    if(me) $.post(BACKEND_URL + '/api/players/delete', {name: me.name, code: me.code})
+      .then(() => {
+        window.localStorage.clear();
+        window.sessionStorage.clear();
+        window.location.href = "/";
+      });
+    else {
+      //TODO: refactor
+      window.localStorage.clear();
+      window.sessionStorage.clear();
+      window.location.href = "/";
+    }
+
   }
 }
